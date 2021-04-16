9587c77e0f60bfc4a7018f5b14cfe4a8c1ec26a4
bfrankie <bfrank0193@gmail.com>
2021-04-16 18:08:55 -0500

    Added Amazon RDS endpoint and credentials to datasource.

--- src/main/java/com/capstone3/api/ApiApplication.java
+++ src/main/java/com/capstone3/api/ApiApplication.java
@@ -1,13 +1,24 @@
 package com.capstone3.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
-
+    @Value("${server.port}")
+    private int portNum;
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner cliRunner() {
+        return args -> {
+            System.out.println(" running on port " + portNum);
+        };
     }
 
 }
